4b0a05d6e865192c9316c30838b7be1c2d1ea38d
JhanAntezana <jhan.antezana.y@uni.pe>
2022-05-16 11:10:33 -0500

    inserción de contenido en tendencias y categorías

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,51 @@
-console.log('API URL = https://luhiulhiusf.com/ahiuhiudaiu?api_jey=' + API_KEY)
+async function getTerndingMoviesPreview(){
+    const response = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await response.json();
+
+    const movies = data.results;
+    console.log(movies)
+    const trendingPreviewContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+    movies.forEach(movie => {
+        const movieContainer = document.createElement('div');
+        const movieImg = document.createElement('img');
+
+        movieContainer.classList.add('movie-container');
+        movieImg.className ='movie-img';
+        movieImg.setAttribute('src','https://image.tmdb.org/t/p/w300'+movie.poster_path);
+        
+        movieContainer.appendChild(movieImg);
+        trendingPreviewContainer.appendChild(movieContainer);
+    });
+
+
+}
+getTerndingMoviesPreview();
+
+getCategoryMovies = async() =>{
+    const response = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await response.json();
+    console.log(data);
+    const categories = data.genres;
+    const trendingCategoriesContainer = document.querySelector('#categories .categories-List');
+    categories.forEach(category => {
+        const categoryContainer = document.createElement('div');
+        categoryContainer.className = 'category-container';
+        const categoryIcon = document.createElement('div');
+        categoryIcon.className = `category-icon ${category.name}`;
+        categoryIcon.id = 'id'+category.id;
+        const categoryIconImg = document.createElement('i');
+        categoryIconImg.className = 'fa-solid fa-clapperboard';
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id',category.name);
+        const categoryTitleText = document.createTextNode(category.name);
+
+        categoryIcon.appendChild(categoryIconImg);
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryIcon);
+        categoryContainer.appendChild(categoryTitle);
+        trendingCategoriesContainer.appendChild(categoryContainer);
+    });
+
+}
+getCategoryMovies();
